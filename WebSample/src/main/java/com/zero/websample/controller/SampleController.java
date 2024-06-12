175838d7fb38081d5c6f0a1ae4140fc48a5143d3
heesane <tees3359@gmail.com>
2024-06-12 23:25:58 +0900

    @Controller

---
+++ WebSample/src/main/java/com/zero/websample/controller/SampleController.java
@@ -0,0 +1,42 @@
+package com.zero.websample.controller;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+public class SampleController {
+
+    @GetMapping("/order/{orderId}")
+    public String getOrder(@PathVariable String orderId){
+        log.info("Get some Order : {}", orderId);
+        return "orderId : "+orderId+", orderAmount:1000";
+    }
+
+    @GetMapping("/order")
+    public String postOrder(
+            @RequestParam String orderId,
+            @RequestParam String orderAmount){
+        log.info("Post some Order Id : {}", orderId);
+        log.info("Post some Order Amount : {}", orderAmount);
+        return "orderId : "+orderId+", orderAmount:"+orderAmount;
+    }
+
+    @PostMapping("/order")
+    public String postOrder2(
+            @RequestBody CreateOrderRequest createOrderRequest,
+            @RequestHeader String userAccountId){
+        log.info("Post some Order Id : {}", createOrderRequest.getOrderId());
+        log.info("Post some Order Amount : {}", createOrderRequest.getOrderAmount());
+        log.info("Post some User Account Id : {}", userAccountId);
+
+        return "User Account Id : "+userAccountId+", orderId : "+createOrderRequest.getOrderId()+", orderAmount:"+createOrderRequest.getOrderAmount();
+    }
+
+    @Data
+    public static class CreateOrderRequest{
+        private String orderId;
+        private Integer orderAmount;
+    }
+}
